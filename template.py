7d94efaf3d7cfa35917a71a489ce0b371558673f
Leonardo Taccari <leonardo.taccari@verizonconnect.com>
2021-12-01 21:04:11 +0100

    Update

--- template.py
+++ template.py
@@ -1,7 +1,8 @@
-import sys
 
-def main(*args):
-    print(*args)
+def main(filename):
+    pass
 
 if __name__ == "__main__":
-    main(*sys.argv[1:])
+    filename = __file__.replace(".py", ".inp")
+    ret = main(filename)
+    print(f"{ret=}") 
