9ae625ee4094881d9afe8b444c15657a95846912
Leonardo Taccari <leonardo.taccari@gmail.com>
2021-12-04 12:00:46 +0100

    Upd

--- 4/day4.py
+++ 4/day4.py
@@ -13,51 +13,24 @@ def read(filename):
 
 def main(filename):
     numbers, boards = read(filename)
-    print(numbers, boards.shape)
-    
-    mask = np.zeros(boards.shape, dtype=bool)
-    for n in numbers:
-        mask = (boards == n) | mask
-        # bingo by dim 1 (a col)
-        b, _ = np.where(mask.sum(axis = 1) == 5)
-        if len(b) > 0:
-            winning_board_idx = b[0]
-            break
-        # bingo by dim 2 (a row)
-        b, _ = np.where(mask.sum(axis = 2) == 5)
-        if len(b) > 0:
-            winning_board_idx = b[0]
-            break
-    
-    winning_board = boards[winning_board_idx]
-    winning_mask = mask[winning_board_idx]
-    print(winning_board, winning_mask, n)
-    score = n*sum(winning_board[~winning_mask])
-    return score
-
-
-def main(filename):
-    numbers, boards = read(filename)
-    
     mask = np.zeros(boards.shape, dtype=bool)
     num_boards = boards.shape[0]
+
     won = set()
     for n in numbers:
         mask = (boards == n) | mask
-        # bingo by dim 1 (a col)
         a, _ = np.where(mask.sum(axis = 1) == 5)
-        # bingo by dim 2 (a row)
         b, _ = np.where(mask.sum(axis = 2) == 5)
         new = (set(a) | set(b)) - won
         won |= new
         
-        if len(won) == num_boards:
-            last = list(new)[0]
+        #if len(won) == 1: #first 
+        if len(won) == num_boards: #last
+            idx = list(new)[0]
             break
             
-    winning_board = boards[last]
-    winning_mask = mask[last]
-    print(winning_board, winning_mask, n)
+    winning_board = boards[idx]
+    winning_mask = mask[idx]
     score = n*sum(winning_board[~winning_mask])
     return score
 
