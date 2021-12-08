ef466e4ef4da60eb1e187af8802ca0ca19c1dbf4
Leonardo Taccari <leonardo.taccari@gmail.com>
2021-12-08 18:05:27 +0100

    Alternative day 6

--- 6/day6.py
+++ 6/day6.py
@@ -1,27 +1,32 @@
-def main1(filename):
-    fish = [int(x) for x in open(filename).read().strip().split(",")]
-    for i in range(80):
-        new = 0
-        for i in range(len(fish)):
-            fish[i] -= 1
-            if fish[i] < 0:
-                fish[i] = 6
-                new += 1
-        fish += [8]*new
-    return len(fish)
+import numpy as np
+from collections import Counter
 
-def main2(filename, gen):
-    import numpy as np
-    fish = [int(x) for x in open(filename).read().strip().split(",")]
-    pop = np.zeros(9, dtype=int)
-    for f in fish:
-        pop[f] += 1
+def main(filename, gen):
+    c = Counter([int(x) for x in open(filename).read().strip().split(",")])
+    fish = np.array([c[i] for i in range(9)])
+
     for i in range(gen):
         pop = np.roll(pop, -1)
         pop[6] += pop[8]
     return pop.sum()
 
+# alternative: use transition matrix
+A = np.block([
+    [np.eye(1,8,6).T, np.eye(8)], 
+    [1, np.zeros((1,8))]
+    ]).astype(int)
+
+def main2(filename, gen):
+    c = Counter([int(x) for x in open(filename).read().strip().split(",")])
+    fish = np.array([c[i] for i in range(9)])
+    return np.sum(np.linalg.matrix_power(A,gen) @ fish)
+
 if __name__ == "__main__":
     filename = __file__.replace(".py", ".inp")
-    ret = main2(filename, gen=256)
-    print(f"{ret=}") 
+    ret1 = main(filename, gen=80)
+    ret2 = main(filename, gen=256)
+    print(ret1, ret2) 
+    
+    ret1 = main2(filename, gen=80)
+    ret2 = main2(filename, gen=256)
+    print(ret1, ret2) 
