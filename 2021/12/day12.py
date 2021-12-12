9d31d4cc54bb303bc67c0090936fc565e8930ad1
Leonardo Taccari <leonardo.taccari@gmail.com>
2021-12-12 11:56:27 +0100

    Day 12

--- 2021/12/day12.py
+++ 2021/12/day12.py
@@ -4,28 +4,25 @@ G = dict()
 def big(x):
     return x.isupper()
 
-def explore(cur, visited, exceptions, path=""):
+def explore(cur, visited, exceptions, path):
     if cur == "end":
         if VERBOSE: print(visited, path)
         return 1
  
     if exceptions > 0:
         return sum(
-                explore(n, visited | {n}, exceptions, path=f"{path}-{n}") 
-                for n in G[cur] 
+                explore(n, visited | {n}, exceptions, path+[n]) for n in G[cur] 
                 if (big(n) or (n not in visited)) and n != "start" ) \
                 + sum(
-                    explore(n, visited | {n}, exceptions-1, path=f"{path}-{n}")
-                    for n in G[cur]
+                    explore(n, visited | {n}, exceptions-1, path+[n]) for n in G[cur]
                     if not big(n) and (n in visited) and n != "start")
     else: # no exceptions remaining
-        return sum(explore(n, visited | {n}, exceptions, path=f"{path}-{n}") 
-                for n in G[cur] 
+        return sum(explore(n, visited | {n}, exceptions, path+[n]) for n in G[cur] 
                 if (big(n) or (n not in visited)) and n != "start" )
 
 def main(filename, exceptions):
     visited = set()
-    return explore("start", visited, exceptions, path="start")
+    return explore("start", visited, exceptions, ["start"])
 
 if __name__ == "__main__":
     filename = __file__.replace(".py", ".inp")
