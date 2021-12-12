9bfb5baa90346aa1a844f56228c8447eb637edec
Leonardo Taccari <leonardo.taccari@gmail.com>
2021-12-12 11:52:35 +0100

    Day 12

--- 2021/12/day12.py
+++ 2021/12/day12.py
@@ -4,39 +4,36 @@ G = dict()
 def big(x):
     return x.isupper()
 
-def explore(cur, visited, again=False, path=""):
-    
-    if again and (big(cur) or visited[cur] == 0):
-        return 0 #no need to use exception
-
-    if visited[cur] == 2 or (visited[cur] == 1 and not again) or (visited[cur] == 1 and cur == "start"):
-        return 0
-
+def explore(cur, visited, exceptions, path=""):
     if cur == "end":
         if VERBOSE: print(visited, path)
         return 1
  
-    visited = visited.copy()
-    if not big(cur):
-        visited[cur] += 1
-    
-    if max(visited.values()) < 2:
-        return sum(explore(n, visited, path=f"{path}-{n}") for n in G[cur]) \
-                + sum(explore(n, visited, again=True, path=f"{path}-{n}") for n in G[cur])
-    else: # already used exception
-        return sum(explore(n, visited, path=f"{path}-{n}") for n in G[cur])
+    if exceptions > 0:
+        return sum(
+                explore(n, visited | {n}, exceptions, path=f"{path}-{n}") 
+                for n in G[cur] 
+                if (big(n) or (n not in visited)) and n != "start" ) \
+                + sum(
+                    explore(n, visited | {n}, exceptions-1, path=f"{path}-{n}")
+                    for n in G[cur]
+                    if not big(n) and (n in visited) and n != "start")
+    else: # no exceptions remaining
+        return sum(explore(n, visited | {n}, exceptions, path=f"{path}-{n}") 
+                for n in G[cur] 
+                if (big(n) or (n not in visited)) and n != "start" )
 
+def main(filename, exceptions):
+    visited = set()
+    return explore("start", visited, exceptions, path="start")
 
-def main(filename):
+if __name__ == "__main__":
+    filename = __file__.replace(".py", ".inp")
     for l in open(filename):
         i,j = l.strip().split("-")
         G.setdefault(i, []).append(j)
         G.setdefault(j, []).append(i)
-    visited = {n:0 for n in G}
-    return explore("start", visited, path="start")
 
-if __name__ == "__main__":
-    filename = __file__.replace(".py", ".inp")
-#    filename = "data2"
-    ret = main(filename)
-    print(f"{ret=}") 
+    ret1 = main(filename, 0)
+    ret2 = main(filename, 1)
+    print(ret1, ret2) 
