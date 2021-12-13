860debe7d3cadaf1a521d4ba6474804c60ae4ae6
Leonardo Taccari <leonardo.taccari@verizonconnect.com>
2021-12-13 12:57:20 +0100

    Day 12

--- 2021/12/day12.py
+++ 2021/12/day12.py
@@ -9,20 +9,12 @@ def explore(cur, visited, exceptions, path):
         if VERBOSE: print(visited, path)
         return 1
  
+    paths = sum(explore(n, visited | {n}, exceptions, path+[n]) for n in G[cur] 
+            if (big(n) or (n not in path)) and n != "start" )
     if exceptions > 0:
-        return sum(
-                explore(n, visited | {n}, exceptions, path+[n]) for n in G[cur] 
-                if (big(n) or (n not in visited)) and n != "start" ) \
-                + sum(
-                    explore(n, visited | {n}, exceptions-1, path+[n]) for n in G[cur]
-                    if not big(n) and (n in visited) and n != "start")
-    else: # no exceptions remaining
-        return sum(explore(n, visited | {n}, exceptions, path+[n]) for n in G[cur] 
-                if (big(n) or (n not in visited)) and n != "start" )
-
-def main(filename, exceptions):
-    visited = set()
-    return explore("start", visited, exceptions, ["start"])
+        paths += sum( explore(n, visited, exceptions-1, path+[n]) for n in G[cur]
+                if not big(n) and (n in visited) and n != "start")
+    return paths
 
 if __name__ == "__main__":
     filename = __file__.replace(".py", ".inp")
@@ -31,6 +23,6 @@ if __name__ == "__main__":
         G.setdefault(i, []).append(j)
         G.setdefault(j, []).append(i)
 
-    ret1 = main(filename, 0)
-    ret2 = main(filename, 1)
+    ret1 = explore("start", set(), exceptions=0, path=["start"])
+    ret2 = explore("start", set(), exceptions=1, path=["start"])
     print(ret1, ret2) 
