475d07efaec7ab69fbc898b4735f787b31c99fd3
Leonardo Taccari <leonardo.taccari@verizonconnect.com>
2021-12-14 19:55:57 +0100

    Day 14 - part 1

--- 2021/14/day14.py
+++ 2021/14/day14.py
@@ -4,7 +4,7 @@ def main(filename):
     lines = open(filename).readlines()
     s = lines[0].strip()
     rules = {tuple(l.split()[0]): l.split()[2].strip() for l in lines[2:]}
-    for i in range(40):
+    for i in range(10):
         s = [c for p in zip(s[:-1],s[1:]) for c in (p[0],rules.get(p,''))] + list(s[-1])
     c = Counter(s).most_common()
     return c[0][1] - c[-1][1] 
