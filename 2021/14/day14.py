73e859c1c7fbeacaca2401d6b0abd4e077dba19e
Leonardo Taccari <leonardo.taccari@verizonconnect.com>
2021-12-14 20:41:30 +0100

    Day 14 - part 2

--- 2021/14/day14.py
+++ 2021/14/day14.py
@@ -1,15 +1,46 @@
-from collections import Counter
+from collections import Counter, defaultdict
 
-def main(filename):
+def parse(filename):
     lines = open(filename).readlines()
     s = lines[0].strip()
-    rules = {tuple(l.split()[0]): l.split()[2].strip() for l in lines[2:]}
-    for i in range(10):
-        s = [c for p in zip(s[:-1],s[1:]) for c in (p[0],rules.get(p,''))] + list(s[-1])
+    rules = {l.split()[0]: l.split()[2].strip() for l in lines[2:]}
+    return s, rules
+
+def part1(filename, N):
+    s, rules = parse(filename)
+    for i in range(N):
+        s = [c for p in zip(s[:-1],s[1:]) for c in (p[0],rules.get(''.join(p),''))] + list(s[-1])
     c = Counter(s).most_common()
     return c[0][1] - c[-1][1] 
 
+def part2(filename, N):
+    s, rules = parse(filename)
+    
+    counts = defaultdict(int)
+    for p in zip(s[:-1],s[1:]):
+        counts["".join(p)] += 1
+
+    for i in range(N):
+        oldcounts = counts.copy()
+        counts = defaultdict(int)
+        for r,v in rules.items():
+            if oldcounts[r] > 0:
+                counts[r[0]+v] += oldcounts[r]
+                counts[v+r[1]] += oldcounts[r]
+
+    letter_counts = defaultdict(int)
+    for p,v in counts.items():
+        letter_counts[p[0]] += v
+        letter_counts[p[1]] += v
+    letter_counts[s[0]] += 1
+    letter_counts[s[-1]] += 1
+
+    assert all(v%2 == 0 for v in letter_counts.values())
+    return (max(letter_counts.values()) - min(letter_counts.values())) // 2
+
 if __name__ == "__main__":
     filename = __file__.replace(".py", ".inp")
-    ret = main(filename)
+    ret = part1(filename, 10)
+    print(f"{ret=}") 
+    ret = part2(filename, 40)
     print(f"{ret=}") 
