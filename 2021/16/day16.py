d393d215fa54bb8cd05539f82f6a9c31d724b37b
Leonardo Taccari <leonardo.taccari@verizonconnect.com>
2021-12-16 19:05:14 +0100

    Day 16 - Part 1

--- 2021/16/day16.py
+++ 2021/16/day16.py
@@ -1,83 +1,103 @@
-HEX = "D2FE28"
 VERBOSE = False
 
-VERSION = 0
-def parse_hex(h):
-    global VERSION
-    VERSION = 0
-    ret = parse(hex2bin(h))
-    print(VERSION)
-    return ret
+class Parser:
+    def __init__(self):
+        self.version = 0
 
-def hex2bin(h):
-    return "".join([f"{int(c,base=16):04b}" for c in h])
+    def parse_hex(self, h):
+        print("#######", h)
+        self.version = 0
+        ret = self.parse_all(self.hex2bin(h))
+        return ret, self.version
+    
+    def hex2bin(self, h):
+        return "".join([f"{int(c,base=16):04b}" for c in h])
 
-def parse(raw, max_depth=None):
-    global VERSION
-    if VERBOSE: print("PARSING:", "".join(list(raw)))
-    if max_depth == 0:
-        return ""
-    match list(raw):
-        case [v0,v1,v2,"1","0","0",*rest]:
-            if VERBOSE: print("literal header:", "".join([v0,v1,v2]), "100")
-            version = int("".join([v0,v1,v2]),base=2)
-            VERSION += version
-            print(f"L, {version=}", VERSION)
-            if max_depth is None:
-                return "(" + literal(rest)
-            return "(" + literal(rest, max_depth=max_depth-1)
-        case [v0,v1,v2,t0,t1,t2,*rest]:
-            version = int("".join([v0,v1,v2]),base=2)
-            VERSION += version
-            print(f"O, {version=}")
-            if VERBOSE: print("operator header:", "".join([v0,v1,v2]), "".join([t0,t1,t2]))
-            return "[" + operator(rest)
-        case _:
-            return ""
+    def parse_all(self, raw):
+        tot = ""
+        remaining = raw
+        while remaining != "":
+            parsed, remaining = self.parse(remaining)
+            tot += parsed
+        return tot
 
-def literal(raw, max_depth=None):
-    """Parse literal *payload* (not header)"""
-    if VERBOSE: print("matching", "".join(list(raw)))
-    match list(raw):
-        case ["1",*rest]:
-            lit = "".join(rest[:4])
-            if VERBOSE: print("literal:", lit, "remaining:", "".join(rest[4:]))
-            return "".join(rest[:4]) + literal(rest[4:], max_depth=max_depth)
-        case ["0", *rest]:
-            lit = "".join(rest[:4])
-            remaining = rest[4:]
-            if VERBOSE: print("literal:", lit, "remaining:", "".join(remaining))
-            if remaining:
-                return lit + ")" + parse(remaining, max_depth=max_depth)
-            return lit
-        case o:
-            print("Literal parsing failed, couldn't match:", o)
-            raise ValueError
+    def parse(self, raw, max_depth=None):
+        """Parse ONE packet (or sub-packet), and what is left.."""
+        if VERBOSE: print("PARSING:", "".join(list(raw)), f"({len(raw)})")
+        match list(raw):
+            case [v0,v1,v2,"1","0","0",*rest]:
+                if VERBOSE: print("literal header:", "".join([v0,v1,v2]), "100")
+                version = int("".join([v0,v1,v2]),base=2)
+                self.version += version
+                if VERBOSE: print(f"L, {version=}", self.version)
+                lit, remaining = self.literal(rest, prefix="(")
+                return lit, remaining
+            case [v0,v1,v2,t0,t1,t2,*rest]:
+                version = int("".join([v0,v1,v2]),base=2)
+                self.version += version
+                if VERBOSE: print(f"O, {version=}", self.version)
+                if VERBOSE: print("operator header:", "".join([v0,v1,v2]), "".join([t0,t1,t2]))
+                op, remaining = self.operator(rest)
+                return op, remaining
+            case _:
+                if VERBOSE: print("All this can't be parsed:", raw)
+                return "", ""
+ 
+   
+    def literal(self, raw, prefix=""):
+        """Parse literal *payload* (not header)"""
+        if VERBOSE: print("matching", "".join(list(raw)))
+        match list(raw):
+            case ["1",*rest]:
+                lit = "".join(rest[:4])
+                if VERBOSE: print("Intermediate chunk of literal:", lit, ", remaining:", "".join(rest[4:]))
+                lit, remaining = self.literal(rest[4:])
+                return prefix + "".join(rest[:4]) + lit, remaining
+            case ["0", *rest]:
+                lit = "".join(rest[:4])
+                remaining = rest[4:]
+                if VERBOSE: print("Last chunk of literal:", lit, ", remaining:", "".join(remaining))
+                return prefix + lit + ")", remaining
+            case o:
+                print("Literal parsing failed, couldn't match:", o)
+                raise ValueError
+    
+    def operator(self, raw):
+        """Parse operator *payload* (not header)"""
+        if VERBOSE: print("matching", "".join(list(raw)))
+        match list(raw):
+            case ["0", *rest]:
+                if len(rest) < 15:
+                    return "", raw
+                length = int("".join(rest[:15]), base=2)
+                if VERBOSE: print("length [0] (num bits):", length)
+                return "[" + self.parse_all(rest[15:15+length]) + "]", rest[15+length:]
+            case ["1", *rest]:
+                subpackets = int("".join(rest[:11]), base=2)
+                if VERBOSE: print("length [1] (num subpackets):", subpackets)
+                remaining = rest[11:]
+                pp = ""
+                for _ in range(subpackets):
+                    parsed, remaining = self.parse(remaining)
+                    pp += parsed
+                return "[" + pp + "]", remaining
+            case o:
+                print("Operator parsing failed, couldn't match:", o)
+                return "", o
 
-def operator(raw):
-    """Parse operator *payload* (not header)"""
-    pass
-    if VERBOSE: print("matching", "".join(list(raw)))
-    match list(raw):
-        case ["0", *rest]:
-            length = int("".join(rest[:15]), base=2)
-            if VERBOSE: print("length [0] (num bits):", length)
-            return parse(rest[15:15+length+1], max_depth=None) + "]"
-        case ["1", *rest]:
-            subpackets = int("".join(rest[:11]), base=2)
-            if VERBOSE: print("length [1] (num subpackets):", subpackets)
-            return parse(rest[11:], max_depth=subpackets) + "]"
-        case o:
-            print("Literal parsing failed, couldn't match:", o)
-            raise ValueError
-
-
-#print(parse_hex(HEX))
-#[1:].strip("()[]"), base=2) == 2021
-#assert int(parse_hex(HEX)[1:].strip("()[]"), base=2) == 2021
+p = Parser()
+print(p.parse_hex("D2FE28"), "011111100101")
+print(p.parse_hex("38006F45291200"), (10,20))
+print(p.parse_hex("EE00D40C823060"), (1,2,3))
+print(p.parse_hex("8A004A801A8002F478"), "exp.version=16")
+print(p.parse_hex("620080001611562C8802118E34"), "exp.version=12")
+print(p.parse_hex("C0015000016115A2E0802F182340"), "exp.version=23")
+print(p.parse_hex("A0016C880162017C3686B18A3D4780"), "exp.version=31")
 
 def main(filename):
-    pass
+    line = open(filename).read()[0]
+    p = Parser()
+    p.parse_hex(line)
 
 if __name__ == "__main__":
     filename = __file__.replace(".py", ".inp")
