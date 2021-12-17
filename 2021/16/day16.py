8917d121e8b3bd9ff2e39d898a7ac41591e70050
Leonardo Taccari <leonardo.taccari@verizonconnect.com>
2021-12-17 15:20:19 +0100

    Some cleanup

--- 2021/16/day16.py
+++ 2021/16/day16.py
@@ -1,3 +1,8 @@
+from functools import reduce
+from dataclasses import dataclass, field
+from typing import List
+from pprint import pprint 
+
 VERBOSE = False
 
 def hex2bin(h):
@@ -6,26 +11,21 @@ def hex2bin(h):
 def bin2dec(*x):
     return int("".join(list(x)),base=2)
 
-from functools import reduce
-from dataclasses import dataclass, field
-from typing import List
-from pprint import pprint 
+def tostr(x):
+    return "".join(x)
 
-OPS = { 0: lambda *z : reduce(lambda x,y: x+y, z, 0),
-        1: lambda *z : reduce(lambda x,y: x*y, z, 1),
+OPS = { 0: sum,
+        1: lambda z : reduce(lambda x,y: x*y, z, 1),
         2: min, 
         3: max, 
-        5: lambda x,y: int(x > y),
-        6: lambda x,y: int(x < y),
-        7: lambda x,y: int(x == y)
+        5: lambda z: int(z[0] > z[1]),
+        6: lambda z: int(z[0] < z[1]),
+        7: lambda z: int(z[0] == z[1])
         }
 
 @dataclass
 class Packet:
     version: int
-    
-    def sum_versions(self):
-        raise NotImplementedError
 
 @dataclass
 class Literal(Packet):
@@ -44,79 +44,63 @@ class Operator(Packet):
 
     @property
     def value(self):
-        if len(self.children) == 0:
-            return 0
-        elif len(self.children) == 1:
-            return self.children[0].value
-        return OPS[self.optype](*[c.value for c in self.children])
+        if self.children:
+            return OPS[self.optype]([c.value for c in self.children])
+        return 0
 
 def parse_hex(h):
-    if VERBOSE: print("######################", h)
+    if VERBOSE: print("##########", h)
     packets = parse_all(hex2bin(h))
     if VERBOSE: pprint(packets)
-    return sum(p.sum_versions() for p in packets), sum([p.value for p in packets])
+    return packets, sum(p.sum_versions() for p in packets), sum([p.value for p in packets])
 
 def parse_all(raw):
     """ Returns list of packets parsed in input string 'raw'"""
     packets = []
     remaining = raw
-    while remaining != "":
+    while remaining:
         packet, remaining = parse_one(remaining)
-        if packet:
-            packets.append(packet)
+        if packet: packets.append(packet)
     return packets
 
 def parse_one(raw):
     """Parse ONE packet (or sub-packet), and return what is left."""
-    if VERBOSE: print("PARSING:", "".join(list(raw)), f"({len(raw)})")
     match list(raw):
         case [v0,v1,v2,"1","0","0",*rest]:
-            if VERBOSE: print("literal header:", "".join([v0,v1,v2]), "100")
-            version = int("".join([v0,v1,v2]),base=2)
+            version = bin2dec(v0,v1,v2)
             lit, remaining = parse_literal(rest)
             return Literal(version=version, value=bin2dec(lit)), remaining
         case [v0,v1,v2,t0,t1,t2,*rest]:
             version = bin2dec(v0,v1,v2)
             optype = bin2dec(t0,t1,t2)
-            if VERBOSE: print("operator header:", "".join([v0,v1,v2]), "".join([t0,t1,t2]))
-            subpackets, remaining = parse_perator(rest)
+            subpackets, remaining = parse_operator(rest)
             return Operator(optype=optype, version=version, children=subpackets), remaining
         case _:
-            if VERBOSE: print("All this can't be parsed:", raw)
             return None, ""
 
 def parse_literal(raw):
     """Parse literal *payload* (not header) and return literal value in binary (plus remaining string)"""
-    if VERBOSE: print("matching", "".join(list(raw)))
     match list(raw):
         case ["1",*rest]:
-            lit = "".join(rest[:4])
-            if VERBOSE: print("Intermediate chunk of literal:", lit, ", remaining:", "".join(rest[4:]))
             lit, remaining = parse_literal(rest[4:])
-            return "".join(rest[:4]) + lit, remaining
+            return tostr(rest[:4]) + lit, remaining
         case ["0", *rest]:
-            lit = "".join(rest[:4])
-            remaining = rest[4:]
-            if VERBOSE: print("Last chunk of literal:", lit, ", remaining:", "".join(remaining))
+            lit, remaining = tostr(rest[:4]), rest[4:]
             return lit, remaining
-        case o:
-            print("Literal parsing failed, couldn't match:", o)
-            raise ValueError
+        case _:
+            raise ValueError("Literal parsing failed, couldn't match:", raw)
 
-def parse_perator(raw):
+def parse_operator(raw):
     """Parse operator *payload* (not header) and return subpackets (+ remaining, unparsed string)"""
-    if VERBOSE: print("matching", "".join(list(raw)))
     match list(raw):
         case ["0", *rest]:
             if len(rest) < 15: return [], raw
             length = int("".join(rest[:15]), base=2)
-            if VERBOSE: print("length [0] (num bits):", length)
             subpackets = parse_all(rest[15:15+length]) 
             return subpackets, rest[15+length:]
         case ["1", *rest]:
             if len(rest) < 11: return [], raw
             num_subpackets = int("".join(rest[:11]), base=2)
-            if VERBOSE: print("length [1] (num subpackets):", num_subpackets)
             remaining = rest[11:]
             subpackets = []
             for _ in range(num_subpackets):
@@ -124,21 +108,19 @@ def parse_perator(raw):
                 subpackets.append(packet)
             return subpackets, remaining
         case _:
-            if VERBOSE: print("Operator parsing failed, couldn't match:", o)
             return [], raw
 
-
-print(parse_hex("D2FE28"), "011111100101")
-print(parse_hex("38006F45291200"), (10,20))
-print(parse_hex("EE00D40C823060"), (1,2,3))
-print(parse_hex("8A004A801A8002F478"), "exp.version=16")
-print(parse_hex("620080001611562C8802118E34"), "exp.version=12")
-print(parse_hex("C0015000016115A2E0802F182340"), "exp.version=23")
-print(parse_hex("A0016C880162017C3686B18A3D4780"), "exp.version=31")
+assert parse_hex("D2FE28")[0][0].value == 2021
+assert parse_hex("8A004A801A8002F478")[1] == 16
+assert parse_hex("620080001611562C8802118E34")[1] == 12
+assert parse_hex("C0015000016115A2E0802F182340")[1] == 23
+assert parse_hex("A0016C880162017C3686B18A3D4780")[1] == 31
+assert parse_hex("9C0141080250320F1802104A08")[2] == 1
 
 def main(filename):
     line = open(filename).read().strip()
-    return parse_hex(line)
+    packets, versions, values = parse_hex(line)
+    return versions, values
 
 if __name__ == "__main__":
     filename = __file__.replace(".py", ".inp")
