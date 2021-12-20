0d3986c147e182b6903c8ef25dd26e20eedd0446
Leonardo Taccari <leonardo.taccari@verizonconnect.com>
2021-12-20 15:42:02 +0100

    Day 19

--- 2021/19/day19.py
+++ 2021/19/day19.py
@@ -1,93 +1,92 @@
 import numpy as np
 from itertools import permutations, product
+
 def parse(filename):
-    scanners = []
+    reports = []
     for l in open(filename):
         if "---" in l:
-            scanners.append([])
+            reports.append([])
         elif l.strip() == "":
             continue
         else:
-            scanners[-1].append([int(x) for x in l.strip().split(",")])
-    return [np.array(s) for s in scanners]
+            reports[-1].append([int(x) for x in l.strip().split(",")])
+    return [np.array(s) for s in reports]
 
+def align(reports):
+    """ Return beacons and scanners, all transformed into the coordinates of scanner 0
+    """
+    scanners = {0: np.array([0,0,0])}
+    
+    aligned_reports = {0: reports[0]} # readings in canonical coordinates (i.e., scanner 0)
+    unprocessed = [0]   # stack of reports in canonical coordinates that
+                        # have been aligned but haven't been used yet to find matches
 
-def do(scanners):
+    while len(aligned_reports) < len(reports):
+        i = unprocessed.pop()
+        for j in range(len(reports)):
+            if j not in aligned_reports:
+                aligned_report, scanner = match_and_align(aligned_reports[i], reports[j])
+                if aligned_report is not None:
+                    print(f"Found alignment between reports {i} and {j}")
+                    aligned_reports[j] = aligned_report
+                    scanners[j] = scanner
+                    unprocessed.append(j)
 
-    scan_0 = {0: scanners[0]} # readings in canonical coordinates (i.e., scanner 0)
-    scan_positions = {0: np.array([0,0,0])}
-    transformed = [0]
-    scan_orig = set(range(1,len(scanners))) # in original coordinates (scanner 1, ...n)
-    while len(scan_0) < len(scanners):
-        s0 = transformed.pop()
-        for i,s in enumerate(scanners):
-            if i not in scan_0: #not yet transformed 
-                ret, scanner_position = find_match(scan_0[s0], s) #try to find correspondence between s0 and s
-                if ret is not None: #managed to bring to canonical coordinates..
-                    scan_0[i] = ret
-                    scan_positions[i] = scanner_position
-                    transformed.append(i)
-                    print(f"Found corr between {s0} and {i}")
-                    print(transformed, scan_0.keys())
-
-    #when everything translated to canonical coordinates..
-
-    stacked = np.vstack(list(scan_0.values()))
+    stacked = np.vstack(list(aligned_reports.values()))
     beacons = np.array(sorted(set(tuple(r) for r in stacked)))
-
-    return beacons, scan_positions 
+    return beacons, scanners 
 
 def manhattan(scanners):
     return max([np.abs(u-v).sum() for i,u in scanners.items() for j,v in scanners.items()])
 
-
-def find_match(scan0, scan1): #transform scan1 in coordinates of scan0, if possible
-    d0, d1 = [{np.linalg.norm(s[i]-s[j]):(i,j) for i in range(len(s)) for j in range(i+1,len(s))} for s in (scan0,scan1)]
+def match_and_align(report0, report1):
+    """ Attempt to find beacons in the overlap of report0 and report1, and 
+    return report1 transformed according to report0 reference system.
+    """
+    # Find clique of beacons that have the same pairwise distances in both reports
+    d0, d1 = [{np.linalg.norm(s[i]-s[j]):(i,j) for i in range(len(s)) for j in range(i+1,len(s))} for s in (report0,report1)]
     common_dist = set(d0) & set(d1)
-
-    indices0 = set([i for c in common_dist for i in d0[c]])
-    indices1 = set([i for c in common_dist for i in d1[c]])
-#    print(indices0, indices1, len(indices0), len(indices1))
-    common_beacons0 = scan0[sorted(indices0)]
-    common_beacons1 = scan1[sorted(indices1)]
-    
+    clique0 = set([i for c in common_dist for i in d0[c]])
+    clique1 = set([i for c in common_dist for i in d1[c]])
+   
+    # Match beacons in report0 and report1 based on the distances to other beacons in the clique
     corr = []
-    for idx in indices0:
-        D = set(np.linalg.norm(scan0[idx]-scan0[j]) for j in indices0)
-        for i in indices1:
-            D1 = set(np.linalg.norm(scan1[i]-scan1[j]) for j in indices1)
+    for idx in clique0:
+        D = set(np.linalg.norm(report0[idx]-report0[j]) for j in clique0)
+        for i in clique1:
+            D1 = set(np.linalg.norm(report1[i]-report1[j]) for j in clique1)
             if D1 == D:
-                print(f"Found correspondence: {idx} (scan in 0-coord) => {i} (other scan)")
+                print(f"Found beacon match: {idx} (report in 0-coord) => {i} (other reportn)")
                 corr.append((idx,i))
                 break
 
+    # If we don't match at least 12 beacons, not enough information to confirm the match
     if len(corr) < 12:
-        print("Not enough correspondences, no good")
         return None, None
 
-    print(corr)
-    a,b = corr[0]
-    c,d = corr[1]
-    for p in permutations(range(3),3):
-        for s in product((-1,1),repeat=3):
-            diff0 = scan0[a]-scan0[c]
-            diff1 = scan1[b]-scan1[d]
+    # Get first two beacons (a,b) to find coordinate transformation + translation
+    # Reorient/rearrange until the diff vector between a and b must be the same in both reports.
+    a0, a1 = corr[0]
+    b0, b1 = corr[1]
+    for p in permutations(range(3), 3):
+        for s in product((-1,1), repeat=3):
+            diff0 = report0[a0] - report0[b0]
+            diff1 = report1[a1] - report1[b1]
             if all(diff1[p,]*s == diff0):
-                print(diff1[p,]*s, diff0, "with transform:", p, s)
                 reorient = p, s
                 break
 
+    # Find position of scanner1 in canonical coordinates (scanner0)
     p, s = reorient
-    translation = scan0[a] - scan1[b,p]*s
-    print(translation,p,s)
+    scanner1 = report0[a0] - report1[a1,p]*s
 
-    scan1_reoriented = np.array([r[p,]*s for r in scan1])
-    scan1_transformed = scan1_reoriented + translation
-    return scan1_transformed, translation
+    report1_reoriented = np.array([r[p,]*s for r in report1])
+    report1_aligned = report1_reoriented + scanner1
+    return report1_aligned, scanner1
 
 def main(filename):
     reports = parse(filename)
-    beacons, scanners = do(reports)
+    beacons, scanners = align(reports)
     return len(beacons), manhattan(scanners)
 
 if __name__ == "__main__":
