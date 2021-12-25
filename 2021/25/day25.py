b859ca1e06822f7cd79917d1be7417ed51fa715e
Leonardo Taccari <leonardo.taccari@verizonconnect.com>
2021-12-25 14:57:53 +0100

    Day 25

--- 2021/25/day25.py
+++ 2021/25/day25.py
@@ -26,9 +26,24 @@ def step(x):
     x = down(x)
     return x
 
+def draw(x, i):
+    from matplotlib import pyplot as plt
+    fig, ax = plt.subplots(1, figsize=(4,4))
+    a = x.copy()
+    a[a=="."] = 0
+    a[a==">"] = 1
+    a[a=="v"] = 2
+    ax.imshow(a.astype(int), vmin=0, vmax=2)
+    ax.axis('off')
+    plt.margins(x=0)
+    plt.savefig(f"img/{i:03d}.png", bbox_inches='tight', pad_inches=0) 
+    plt.close()
+    plt.cla()
+
 def main(filename):
     cucumbers = np.array([list(l.strip()) for l in open(filename)])
-    for i in trange(4000):
+    for i in trange(1000):
+        draw(cucumbers, i)
         new = step(cucumbers)
         if (new == cucumbers).all(): break
         cucumbers = new
